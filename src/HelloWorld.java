a65b3393299a150c3fcac5c08cd50f187ffd75ec
trd6577 <trd6577@g.rit.edu>
2015-06-16 10:32:24 -0400

    HelloWorld.java   commented

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,22 @@
 /**
+ * File: HelloWorld.java
  * Created by TRDan6577
+ * Purpose: Serves as an introduction to the way to use github; a simple starting program so that I understand the
+ *          basics before I begin to push and pull my larger, more important programs/projects
+ */
+
+/**
+ * Note: DO NOT INSTANTIATE THIS CLASS
+ * Purpose: Prints "Hello World!"
  */
 public class HelloWorld {
 
+    /**
+     * Purpose: The main runner for the program. Prints "Hello World!" to standard output
+     * @param args (String []) Not used
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 
-}
+} // End HelloWorld
